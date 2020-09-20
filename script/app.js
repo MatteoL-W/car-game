6d1ef43b0fea01fcf042d487c3d9e880d60ddd1e
Matteo <matteo.ftt@gmail.com>
2020-09-20 18:48:00 +0200

    début de randomcar

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+/* VARIABLES */
+
 let canvas = document.querySelector('canvas'),
     c = canvas.getContext('2d'),
     gamePlaying = true,
@@ -5,7 +7,15 @@ let canvas = document.querySelector('canvas'),
     speed = 2,
     bestScore = 0,
     clientX,
-    clientY;
+    clientY,
+    voitures = [
+        [1366, 0],
+        [1366, 256],
+        [1366, 512],
+        [1622, 0],
+        [1366, 256]
+    ],
+    emplacement = [110, 408, 700, 998];
 
 const img = new Image();
       img.src = 'assets/canvas/bg-default.png';
@@ -14,18 +24,20 @@ const img = new Image();
 canvas.width = 1366;
 canvas.height = 766;
 
-setInterval(() => speed = speed * 1.0001, 100);
+/* AUGMENTATION DE LA VITESSE */
+setInterval(() => speed = speed * 1.001, 100);
 
 const render = () => {
     index++;
-
 
     /* BACKGROUND */
     c.drawImage(img, 0, 0, canvas.width, canvas.height, 0, (index * speed) % canvas.height - canvas.height, canvas.width, canvas.height);
     c.drawImage(img, 0, 0, canvas.width, canvas.height, 0, (index * speed) % canvas.height, canvas.width, canvas.height);
 
-    /* CAR */
+    /* MY CAR */
     c.drawImage(img, 1366, 0, 256, 256, voitureClientX(), 460 /* hauteur figé*/, 256, 256);
+
+    c.drawImage(img, ...voitures[Math.floor(Math.random() * 5)] , 256, 256, emplacement[Math.floor(Math.random() * 4)] + (window.innerWidth - canvas.width)/2 /*x*/, 0 /*y*/, 256, 256);
 
     /* CURSOR */
     c.beginPath();
@@ -35,10 +47,12 @@ const render = () => {
     c.fill();
     c.stroke();
 
+    
 
     window.requestAnimationFrame(render);
 }
 
+/* MOUSEMOVE */
 document.addEventListener("mousemove", function(e) {
     if (gamePlaying) {
         clientX = e.clientX;
@@ -47,15 +61,25 @@ document.addEventListener("mousemove", function(e) {
     //e.clientX > 190 && e.clientX < 1336
 });
 
+/* NE PAS DEPASSER DU CADRE */
 function voitureClientX() {
-    if (clientX < 190) return 190 - 128 - (window.innerWidth - canvas.width)/2;
-    else if (clientX > 1336) return 1336 - 128 - (window.innerWidth - canvas.width)/2;
+    if (clientX < 100 + (window.innerWidth - canvas.width)/2) return 100 + (window.innerWidth - canvas.width)/2 - 128 - (window.innerWidth - canvas.width)/2;
+    else if (clientX > 1300 + (window.innerWidth - canvas.width)/2) return 1300 + (window.innerWidth - canvas.width)/2 - 128 - (window.innerWidth - canvas.width)/2;
     else return clientX - 128 - (window.innerWidth - canvas.width)/2;
 }
 
-img.onload = render;
+function randomCar() {
+   
+    //c.drawImage(img, ...voitures[Math.floor(Math.random() * 5)] , 256, 256, emplacement[Math.floor(Math.random() * 4)] + (window.innerWidth - canvas.width)/2 /*x*/, 0 /*y*/, 256, 256);
+}
 
-function randomCar() {}
 function randomX() {}
 // setInterval tous les 2s pour faire apparaitre une voiture avec APPARENCE RANDOM et un RANDOM X
 // MEILLEUR SCORE (avec index ?)
+
+img.onload = render;
+
+
+
+
+
