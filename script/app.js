17a3319f88fd1891166fd8ff28d1eb3aecb552f4
MatteoL-petitordi <matteo.ftt@gmail.com>
2020-10-18 14:29:24 +0200

    My Car changeable, manque des obstacles et OK

--- script/app.js
+++ script/app.js
@@ -1,5 +1,18 @@
 /* VARIABLES */
 
+const img = new Image();
+      img.src = 'assets/canvas/bg-default.png',
+      audio_explosion = new Audio("assets/sound/explosion.mp3"),
+      voitures = [
+        [1366, 0],
+        [1366, 256],
+        [1366, 512],
+        [1622, 0],
+        [1622, 256],
+        [1878, 0]],
+    emplacements = [115,415,715,1015],
+    divVoitures = document.getElementById('voitures');
+
 let canvas = document.querySelector('canvas'),
     c = canvas.getContext('2d'),
     gamePlaying = false,
@@ -8,32 +21,18 @@ let canvas = document.querySelector('canvas'),
     bestScore = 0,
     clientX,
     clientY,
-    voitures = [
-        [1366, 0],
-        [1366, 256],
-        [1366, 512],
-        [1622, 0],
-        [1622, 256]
-    ],
-    emplacements = [115,415,715,1015],
     emplacement = [],
     vitesse = [],
     voiture = [],
     score,
-    audio_explosion = new Audio("assets/sound/explosion.mp3"),
-    soustraction,
-    init = false;
-
-const img = new Image();
-      img.src = 'assets/canvas/bg-default.png';
+    init = false,
+    myCar = voitures[0];
 
 
 canvas.width = 1366;
 canvas.height = 766;
 
 /* AUGMENTATION DE LA VITESSE */
-
-//setInterval(() => speed = speed * 1.0001, 100)
 
 const render = () => {
     
@@ -45,21 +44,19 @@ const render = () => {
         c.drawImage(img, 0, 0, canvas.width, canvas.height, 0, (index * speed * 2) % canvas.height, canvas.width, canvas.height);
     
         /* MY CAR */
-        c.drawImage(img, 1366, 0, 256, 256, voitureClientX(), 460 /* hauteur figé*/, 256, 256);
-    
+        c.drawImage(img, ...myCar, 256, 256, voitureClientX(), 460 /* hauteur figé*/, 256, 256);
     
         /* CURSOR */
         c.beginPath();
         c.arc(clientX - (window.innerWidth - canvas.width)/2, clientY, 10, 0, 2 * Math.PI);
         c.fillStyle='white';
-        c.strokeStyle='white'
+        c.strokeStyle='white';
         c.fill();
         c.stroke();
     
         c.font = "48px Calibri";
     
         if (init) {
-
             let hauteur = speed*index*3 % (canvas.height+250) - 250;
 
             for (let i = 1; i < 4; i++) {
@@ -68,10 +65,13 @@ const render = () => {
                 // SI COLLISION
                 if (hauteur > 260 && hauteur < 670) {
                     if (voitureClientX() > emplacements[emplacement[i]] - 128 && voitureClientX() < emplacements[emplacement[i]] + 128) {
+                        
                         c.drawImage(img, 1622, 512, 256, 256, voitureClientX(), 460, 256, 256);
                         audio_explosion.play();
-                        gamePlaying = false;
                         score = 0;
+                        divVoitures.style.display='block';
+                        gamePlaying = false;
+
                     }
                 }
             }
@@ -80,19 +80,25 @@ const render = () => {
                 init = 0;
                 setup();
             }
-
-        } else {
-
         }
-            // en gros quand la hauteur est identique on regarde si notre voiture fait parti des emplacements
 
-        
         bestScore = Math.floor(Math.max(bestScore, index/2));
         score = Math.floor(index/2);
     
         c.fillText("Meilleur score: " + bestScore,120,50);
         c.fillText("Score actuel: " + score,120,100);
 
+    } else {
+        index = 0;
+        c.fillStyle = '#528e2b';
+        c.fillRect(0,0,canvas.width,canvas.height);
+        
+        c.fillStyle = 'white';
+        c.font = "40px Calibri";
+        c.fillText("Votre meilleur score: " + bestScore,50,100);
+        c.fillText("Cliquez sur votre voiture pour commencer ", canvas.width / 2 - 350, canvas.height /2 + 300);
+
+        canvas.style.cursor = 'pointer';
     }
 
     window.requestAnimationFrame(render);
@@ -114,30 +120,32 @@ function voitureClientX() {
 }
 
 function setup() {
-    init = true;
-    emplacement = [];
-    let nombre = [0,1,2,3];
-
-    for (let i = 1; i < 5; i++) {
-        voiture[i] = Math.floor(Math.random() * voitures.length);
+    canvas.style.cursor = 'none';
+    if (gamePlaying == true) {
+        init = true;
+        emplacement = [];
+        let nombre = [0,1,2,3];
+    
+        for (let i = 1; i < 6; i++) {
+            voiture[i] = Math.floor(Math.random() * voitures.length);
+        }
+    
+        for (let i = 0; i < 4; i++) {
+            let aleat = Math.floor(Math.random() * nombre.length);
+            emplacement[i] = nombre[aleat];
+            nombre.splice(aleat,1);
+        }
     }
+}
 
-    for (let i = 0; i < 4; i++) {
-        let aleat = Math.floor(Math.random() * nombre.length);
-        emplacement[i] = nombre[aleat];
-        nombre.splice(aleat,1);
-    }
-
-
+function changerMyCar(valeur) {
+    myCar = voitures[valeur];
+    divVoitures.style.display='none';
+    gamePlaying = true;
+    setup();
 
 }
 
-canvas.addEventListener('click', () => {
-    if (gamePlaying == false) {
-        gamePlaying = true;
-        setup();
-    }
-});
 
 img.onload = render;
 
