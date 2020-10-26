091b6421dd2e009db91774ce91a5fde8277ff1e3
MatteoL-petitordi <matteo.ftt@gmail.com>
2020-10-26 13:23:38 +0100

    Difficulte ajoute

--- script/app.js
+++ script/app.js
@@ -11,7 +11,9 @@ const img = new Image();
         [1622, 256],
         [1878, 0]],
     emplacements = [115,415,715,1015],
-    divVoitures = document.getElementById('voitures');
+    emplacementsTP = [265,557,848],
+    divVoitures = document.getElementById('voitures'),
+    divDifficultes = document.getElementById('difficultes');
 
 let canvas = document.querySelector('canvas'),
     c = canvas.getContext('2d'),
@@ -25,6 +27,7 @@ let canvas = document.querySelector('canvas'),
     vitesse = [],
     voiture = [],
     score,
+    difficulteTab = [1,2,2], // facile par défaut
     init = false,
     myCar = voitures[0];
 
@@ -57,7 +60,17 @@ const render = () => {
         c.font = "48px Calibri";
     
         if (init) {
-            let hauteur = speed*index*3 % (canvas.height+250) - 250;
+            let hauteur = speed*index*difficulteTab[2] % (canvas.height+250) - 250;
+
+            for (let i = 0; i < 3; i++) {
+                c.drawImage(img, 1878, 256, 256, 256, emplacementsTP[i], hauteur, 256, 256);
+
+                if (hauteur > 260 && hauteur < 670) {
+                    if (voitureClientX() > emplacementsTP[i] - 50 && voitureClientX() < emplacementsTP[i] + 50) {
+                        perdu();
+                    }
+                }
+            }
 
             for (let i = 1; i < 4; i++) {
                 c.drawImage(img, ...voitures[voiture[i]] , 256, 256, emplacements[emplacement[i]] /*x*/, hauteur /*y*/, 256, 256);
@@ -65,19 +78,17 @@ const render = () => {
                 // SI COLLISION
                 if (hauteur > 260 && hauteur < 670) {
                     if (voitureClientX() > emplacements[emplacement[i]] - 128 && voitureClientX() < emplacements[emplacement[i]] + 128) {
-                        
-                        c.drawImage(img, 1622, 512, 256, 256, voitureClientX(), 460, 256, 256);
-                        audio_explosion.play();
-                        score = 0;
-                        divVoitures.style.display='block';
-                        gamePlaying = false;
-
+                        perdu();
                     }
                 }
             }
 
             if (hauteur < -240) {
                 init = 0;
+                if ( difficulteTab[2] >= 4 ) {
+                    difficulteTab[2] = difficulteTab[2] + 0.05;
+                }
+                difficulteTab[2] = difficulteTab[2] + 0.02;
                 setup();
             }
         }
@@ -114,36 +125,75 @@ document.addEventListener("mousemove", function(e) {
 
 /* NE PAS DEPASSER DU CADRE */
 function voitureClientX() {
-    if (clientX < 100 + (window.innerWidth - canvas.width)/2) return 100 + (window.innerWidth - canvas.width)/2 - 128 - (window.innerWidth - canvas.width)/2;
+    if (clientX < 150 + (window.innerWidth - canvas.width)/2) return 150 + (window.innerWidth - canvas.width)/2 - 128 - (window.innerWidth - canvas.width)/2;
     else if (clientX > 1250 + (window.innerWidth - canvas.width)/2) return 1250 + (window.innerWidth - canvas.width)/2 - 128 - (window.innerWidth - canvas.width)/2;
     else return clientX - 128 - (window.innerWidth - canvas.width)/2;
 }
 
 function setup() {
     canvas.style.cursor = 'none';
+
     if (gamePlaying == true) {
         init = true;
         emplacement = [];
-        let nombre = [0,1,2,3];
+
+        let nombre = [0,1,2,3],
+            nombres2 = [0,1,2];
     
         for (let i = 1; i < 6; i++) {
             voiture[i] = Math.floor(Math.random() * voitures.length);
         }
     
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < difficulteTab[0] + 1; i++) {
             let aleat = Math.floor(Math.random() * nombre.length);
             emplacement[i] = nombre[aleat];
             nombre.splice(aleat,1);
         }
+
+        for (let i = 2; i < 2; i++) {
+            let aleat = Math.floor(Math.random() * nombre2.length);
+            emplacementsTP[i] = nombre2[aleat];
+            nombre2.splice(aleat,1);
+        }
     }
+}
+
+function changerDifficulte(difficulte) {
+    if (difficulte == "Facile") {
+        // sous la forme [nombreDeVoitures, speedDefault,speedChangeante]
+        difficulteTab = [1,2.1,2.1];
+    }
+
+    else if (difficulte == "Moyen") {
+        difficulteTab = [2,2.3,2.3];
+    }
+
+    else if (difficulte == "Dur") {
+        difficulteTab = [3,2.5,2.5];
+    }
+    
+    else if (difficulte == "Extreme") {
+        difficulteTab = [3,4,4];
+    }
+    return difficulteTab;
 }
 
 function changerMyCar(valeur) {
     myCar = voitures[valeur];
     divVoitures.style.display='none';
+    divDifficultes.style.display='none';
     gamePlaying = true;
+    difficulteTab[2] = difficulteTab[1];
     setup();
+}
 
+function perdu() {
+    c.drawImage(img, 1622, 512, 256, 256, voitureClientX(), 460, 256, 256);
+    audio_explosion.play();
+    score = 0;
+    divVoitures.style.display='block';
+    divDifficultes.style.display='flex';
+    gamePlaying = false;
 }
 
 
